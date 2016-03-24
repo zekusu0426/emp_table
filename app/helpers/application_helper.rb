bacf651cf18ab72dca8648d56006b8faad11ddd7
root <root@localhost.localdomain>
2016-03-24 16:47:25 +0900

    update form validate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def active?(controller_name)
+     return "active" if controller_name == params[:controller]
+  end
 end
--- app/models/jigyosho.rb
+++ app/models/jigyosho.rb
@@ -16,25 +16,46 @@
 #
 
 class Jigyosho < ActiveRecord::Base
-  validates :cd, presence: true
-  validates :cd, length: { in: 1..10 }, allow_blank: true
+  attr_accessor :year, :month, :day
+  class YmdIsCorrectValidator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
 
-  validates :name, presence: true
-  validates :name, length: { in: 1..60 }, allow_blank: true
+      def year_correct?(year)
+        return ( 1000 <= year ) && ( year <= 2999 )
+      end
 
-  validates :ryakusho, presence: true
-  validates :ryakusho, length: { in: 1..30 }, allow_blank: true
+      def month_correct?(month)
+        return ( 1 <= month ) && ( month <= 12 )
+      end
 
-  validates :from_ymd, presence: true
-  validates :from_ymd, length: { is: 8 }, allow_blank: true
+      def day_correct?(day)
+        return ( 1 <= day ) && ( day <= 31 )
+      end
 
-  validates :to_ymd, length: { is: 8 }, allow_blank: true
+      ymd = value.to_s
+      year = ymd[0..3].to_i
+      month = ymd[4..5].to_i
+      day = ymd[6..7].to_i
+
+      unless (year_correct?(year) && month_correct?(month) && day_correct?(day))
+        record.errors[attribute] << I18n.t('errors.messages.invalid')
+      end
+    end
+  end
+
+  validates :cd, presence: true, uniqueness: true, length: { in: 1..10 }
+  
+  validates :name, presence: true, length: { in: 1..60 }
+
+  validates :ryakusho, presence: true, length: { in: 1..30 }
+
+  validates :from_ymd, presence: true, length: { is: 8 }, ymd_is_correct: true
+
+  validates :to_ymd, length: { is: 8 }, ymd_is_correct: true, allow_blank: true
 
   validates :mail, length: { in: 1..50 }, allow_blank: true
 
   validates :sort_num, length: { in: 1..10 }, allow_blank: true
 
-  validates :flg, presence: true
-  validates :flg, length: { in: 1..2 }, allow_blank: true
-
+  validates :flg, presence: true, length: { in: 1..2 }
 end
